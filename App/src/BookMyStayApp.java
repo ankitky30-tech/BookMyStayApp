b704cbafdcf0b12a093779dfd426ae60de4078c6
ankitky30-tech <ankitky30@gmail.com>
2026-03-30 14:48:08 +0530

    UC1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,8 @@
+
+public class BookMyStayApp {
+    static void main() {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System Initialized Successfully.");
+    }
+
+}
